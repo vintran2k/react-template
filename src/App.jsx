d287d32db1ead82ffa30a0eb8c938a215afd6968
vintran <vintran@student.42.fr>
2023-02-02 19:26:48 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={ <Home /> } />
-				<Route path="/home" element={ <Home /> } />
-				<Route path="/about" element={ <About /> } />
+				<Route path="/" element={ <Home /> } >
+					<Route path="/about" element={ <About /> } />
+				</Route>
 				<Route path="*" component={<Home />} />
 			</Routes>
 		</BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
 	return (
 		<div className="Home">
 			<Navbar />
 			<div className="Home-content">
-				
+				<Outlet />
 			</div>
 		</div>
 	);
